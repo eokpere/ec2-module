5670b5f7e790d1b63dff176f15fa433b2d4f33c8
Evelyn Okpere <113376902+eokpere@users.noreply.github.com>
2023-01-31 14:01:32 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,12 +10,12 @@ provider "aws" {
 
 #create sg
 module "security_group" {
-  source = "../modules-sg-ec2/security-group-ssh"
+  source = "/modules-sg-ec2/security-group-ssh"
 }
 
 #create ec2 instance
 module "ec2_instance" {
-  source                        = "../modules-sg-ec2/Ec2"
+  source                        = "/modules-sg-ec2/Ec2"
   instance_type                 = var.instance_type
   #key_name                      = var.key_name
 }
