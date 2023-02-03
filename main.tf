05284a8d6726841f1b4c61842cdc4d44397cec4c
eokpere <evelynokpere@outlook.com>
2023-02-02 21:21:10 -0500

    added tag

--- main.tf
+++ main.tf
@@ -13,4 +13,7 @@ module "ec2_instance" {
   source                        = "./modules-sg-ec2/Ec2"
   instance_type                 = var.instance_type
   #key_name                      = var.key_name
+  tags = {
+        Name = var.ec2name
+    }
 }
