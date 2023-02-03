ca2fa187b3650f636af7ef1c61b854a7a82f6017
eokpere <evelynokpere@outlook.com>
2023-02-02 21:57:39 -0500

    updates

--- modules-sg-ec2/Ec2/variables.tf
+++ modules-sg-ec2/Ec2/variables.tf
@@ -4,13 +4,12 @@ variable "instance_type" {
   type        = string
   default     = null
 }
-/*
 variable "ec2Name" {
   description = "name of the instance"
   type        = string
   default     = null
 }
-*/
+
 variable "key_name" {
   description = "Key name of the Key Pair to use for the instance; which can be managed using the `aws_key_pair` resource"
   type        = string
